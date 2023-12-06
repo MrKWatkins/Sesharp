3c2eed1bd7ab47d40a8f5444d4fbc202da3e7cf7
MrKWatkins <mrkwatkins@gmail.com>
2023-12-06 21:41:48 +0000

    NuGet updates.

--- MrKWatkins.DocGen.Console/Program.cs
+++ MrKWatkins.DocGen.Console/Program.cs
@@ -1,4 +1,4 @@
-﻿using MrKWatkins.DocGen.Markdown.Generation;
+using MrKWatkins.DocGen.Markdown.Generation;
 using MrKWatkins.DocGen.Model;
 using MrKWatkins.DocGen.XmlDocumentation;
 using Assembly = System.Reflection.Assembly;
@@ -12,10 +12,4 @@ var assembly = Assembly.LoadFile(assemblyPath);
 
 var model = AssemblyParser.Parse(assembly, documentation);
 
-var ast = model.Descendents.First(n => n.Name == "MrKWatkins.Ast");
-
-var node = model.Descendents.First(n => n.Name == "Node`1");
-
 AssemblyMarkdownGenerator.Generate(model, "/home/mrkwatkins/DocGenOutput");
-
-return;
